45529abc14115556bb4fbf79defa2188fd6993c5
zhenya <zhenya@MacBook-Pro-zhenya.local>
2017-03-29 13:10:49 +0300

    add routing and page templates

--- frontend/payment/src/app/app.module.ts
+++ frontend/payment/src/app/app.module.ts
@@ -2,19 +2,58 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule }   from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { OrderComponent } from './order/order.component';
+import { MainComponent } from './main/main.component';
+import { ShippingComponent } from './shipping/shipping.component';
+import { PaymentComponent } from './payment/payment.component';
+import { ReviewComponent } from './review/review.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OrderComponent,
+    MainComponent,
+    ShippingComponent,
+    PaymentComponent,
+    ReviewComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        redirectTo: 'main',
+        pathMatch: 'full'
+      },
+      {
+        path: 'main',
+        component: MainComponent
+      },
+      {
+        path: 'order',
+        component: OrderComponent
+      },
+      {
+        path: 'shipping',
+        component: ShippingComponent
+      },
+      {
+        path: 'payment',
+        component: PaymentComponent
+      },
+      {
+        path: 'review',
+        component: ReviewComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
---
+++ frontend/payment/src/app/review/review.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-review',
+  templateUrl: './review.component.html'
+})
+export class ReviewComponent {
+  title = 'app works!';
+}
