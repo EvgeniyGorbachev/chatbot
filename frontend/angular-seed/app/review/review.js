bac6366907e0db1e4ef5cf8e2611a654344fe59b
zhenya <zhenya@MacBook-Pro-zhenya.local>
2017-04-10 14:45:55 +0300

    close empty profile sending

--- frontend/angular-seed/app/review/review.js
+++ frontend/angular-seed/app/review/review.js
@@ -22,18 +22,22 @@ angular.module('myApp.review', ['ngRoute'])
 
   vm.save = function () {
 
-    userService.sendData(userService.getUser())
-      .then(function (res) {
+    if (userService.getUser().isPaymentCompleted) {
+      userService.sendData(userService.getUser())
+        .then(function (res) {
 
-        if (res.status == 200) {
-          vm.buttonText = ' Complete successfully!';
-          vm.user.isPaymentCompleted = true;
-          userService.saveUser(vm.user);
-        } else {
+          if (res.status == 200) {
+            vm.buttonText = ' Complete successfully!';
+            vm.user.isPaymentCompleted = true;
+            userService.saveUser(vm.user);
+          } else {
+            vm.buttonText = ' Payment error';
+          }
+        }, function (res) {
           vm.buttonText = ' Payment error';
-        }
-      }, function (res) {
-        vm.buttonText = ' Payment error';
-      });
+        });
+    } else {
+      $location.path( '/main' );
+    }
   };
 }]);
