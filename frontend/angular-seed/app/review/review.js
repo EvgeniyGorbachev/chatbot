b09f232592736a029d9619861ff727b55296ba43
zhenya <zhenya@MacBook-Pro-zhenya.local>
2017-04-12 12:01:38 +0300

    payment button add logic

--- frontend/angular-seed/app/review/review.js
+++ frontend/angular-seed/app/review/review.js
@@ -14,27 +14,28 @@ angular.module('myApp.review', ['ngRoute'])
   var vm = this;
 
   vm.user = userService.getUser();
-  vm.buttonText = ' Complete Payment';
+  vm.buttonText = null;
+  vm.isDataSend = false;
 
   vm.go = function ( path ) {
     $location.path( path );
   };
 
   vm.save = function () {
+    vm.isDataSend = true;
+    vm.buttonText = true;
 
-  userService.sendData(userService.getUser())
-    .then(function (res) {
+    userService.sendData(userService.getUser())
+      .then(function (res) {
 
-      if (res.status == 200) {
-        vm.buttonText = ' Complete successfully!';
-        vm.user.isPaymentCompleted = true;
-        userService.saveUser(vm.user);
-      } else {
-        vm.buttonText = ' Payment error';
-      }
-    }, function (res) {
-      vm.buttonText = ' Payment error';
+        if (res.status == 200) {
+          vm.user.isPaymentCompleted = true;
+          userService.saveUser(vm.user);
+        } else {
+          vm.buttonText = false;
+        }
+      }, function (res) {
+        vm.buttonText = false;
     });
-
   };
 }]);
