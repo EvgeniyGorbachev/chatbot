442e8d12fa2abae27f9b9e3a0225a72a95c10867
zhenya <webvagus@gmail.com>
2017-04-20 14:34:01 +0300

    add campaign files

---
+++ backend/assets/js/controllers/dashboard.js
@@ -0,0 +1,15 @@
+'use strict';
+
+angular.module('campaignsApp', [])
+  .controller('DashboardListController', function() {
+
+    let vm = this;
+    vm.change = change;
+    vm.isActive = {};
+
+    function change(id) {
+      let bool = vm.isActive[id] || false;
+      $('#data').val(JSON.stringify({"id": id, "isActive": bool}));
+      $('form').submit();
+    }
+  })
