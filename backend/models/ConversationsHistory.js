82bfc0c32140cfe45f6aa58a4dac153415891867
Mihail Shumilov <mschumilow@gmail.com>
2017-04-27 17:07:49 +0300

    add conversation history

---
+++ backend/models/ConversationsHistory.js
@@ -0,0 +1,51 @@
+var Sequelize = require("sequelize")
+
+module.exports = function (sequelize, DataTypes)
+{
+    return sequelize.define('ConversationsHistory', {
+        id         : {
+            type         : Sequelize.INTEGER,
+            primaryKey   : true,
+            autoIncrement: true
+        },
+        user_id    : {
+            type     : Sequelize.STRING,
+            allowNull: false
+        },
+        campaign_id: {
+            type     : Sequelize.INTEGER,
+            allowNull: false
+        },
+        text       : {
+            type: Sequelize.TEXT
+        },
+        direction  : {
+            type: Sequelize.INTEGER
+        },
+        createdAt  : {
+            type: Sequelize.DATE
+        },
+        updatedAt  : {
+            type: Sequelize.DATE
+        }
+    }, {
+        individualHooks: true,
+        underscored    : true,
+        tableName      : 'conversation_history',
+        validate       : {
+            isJSON: function ()
+            {
+                // try {
+                //   JSON.parse(this.shipping)
+                // } catch (e) {
+                //   throw new Error('shipping contains not valid json')
+                // }
+                // try {
+                //   JSON.parse(this.billing)
+                // } catch (e) {
+                //   throw new Error('billing contains not valid json')
+                // }
+            }
+        }
+    })
+}
