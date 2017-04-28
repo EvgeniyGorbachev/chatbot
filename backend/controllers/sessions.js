4658b3712b0d87f792f0281c43488f27d2b7add3
Mihail Shumilov <mschumilow@gmail.com>
2017-04-28 17:13:48 +0300

    try add

--- backend/controllers/sessions.js
+++ backend/controllers/sessions.js
@@ -1,18 +1,23 @@
-const db = require('../models/index.js')
+const db     = require('../models/index.js')
 const Smooch = require('smooch-core')
+let jwt      = require('jsonwebtoken');
 
 /**
  * GET /sessions/:id
  * Get sessions by id.
  */
-exports.getSessionsById = (req, res) => {
-  let campaignId = req.params.campaignid || false
-  db.Campaigns.findOne({where: {id: campaignId}}).then(function (campaign) {
-    res.render('sessions', {
-                campaign : campaign,
-              })
+exports.getSessionsById = (req, res) =>
+{
+    let campaignId = req.params.campaignid || false
+    db.Campaigns.findOne({where: {id: campaignId}}).then(function (campaign)
+    {
+        res.render('sessions', {
+            campaign: campaign,
+            jwt     : jwt.sign({scope: 'app'}, campaign.smooch_app_secret)
+        })
 
-  }).catch(function(err) {
-    res.redirect('/campaigns')
-  })
+    }).catch(function (err)
+    {
+        res.redirect('/campaigns')
+    })
 };
