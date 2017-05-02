9e7f93daee2c8b79121498ee027bdd9109ee8f7c
zhenya <webvagus@gmail.com>
2017-05-02 18:05:28 +0300

    add iframe to smooch chat window

---
+++ backend/assets/js/smooch.js
@@ -0,0 +1,80 @@
+jQuery(document).ready(function () {
+  let token = $('#smooch_app_token').val();
+
+  // Init Smooch messenger
+  Smooch.init({appToken: token}).then(function () {
+
+    // Listen new messages
+    $(".send").on('click', function() {
+      checkMessages(getLastMessages());
+    });
+
+    $('.message-input').keyup(function(e){
+      if(e.keyCode == 13) {
+        checkMessages(getLastMessages());
+      }
+    });
+
+    // Check all old messages
+    $.each(getAllMessages(), function(index, mes) {
+      checkMessages(mes);
+    });
+
+  });
+
+  // Check for links and add iframe instead of all links.
+  function checkMessages(mes) {
+    let messageJqueryObject = $(mes);
+
+    let html = '';
+    let iFrame = '';
+    let messageText = messageJqueryObject.text();
+    let textArray = messageText.split(' ');
+
+    // Checking each word in the sentence
+    $.each(textArray, function(index, text) {
+      // If valid link
+      if (/^(ftp|http|https):\/\/[^ "]+$/.test(text)) {
+
+        let youtubeId = getYoutubeId(text);
+
+        if (youtubeId != 'error') {
+          iFrame += '<iframe width="200" height="200" src="//www.youtube.com/embed/' + youtubeId + '" frameborder="0" allowfullscreen></iframe>';
+        } else {
+          iFrame += '<iframe width="200" height="200" src="' + text + '" frameborder="0" allowfullscreen></iframe>';
+        }
+
+        // If link too long, replace it
+        if (text.length > 60) {
+          textArray[index] = '<strong><a href="' + text + '" target="_blank">link</a></strong>';
+        }
+      }
+    });
+
+    html = textArray.join(" ");
+    html += iFrame;
+    // Add new messages html
+    messageJqueryObject.find('.sk-message-item').html(html);
+  }
+
+  function getLastMessages() {
+    return $('.sk-messages').children().last();
+
+  }
+
+  function getAllMessages() {
+    return $('.sk-messages').children();
+  }
+
+  function getYoutubeId(url) {
+    let regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
+    let match = url.match(regExp);
+
+    if (match && match[2].length == 11) {
+      return match[2];
+    } else {
+      return 'error';
+    }
+  }
+
+});
