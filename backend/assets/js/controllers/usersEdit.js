03b75594b24e489fe9eb060a2ecab31dcf7d6bd2
Mihail Shumilov <mschumilow@gmail.com>
2017-05-09 14:23:43 +0300

    fix dropdown

--- backend/assets/js/controllers/usersEdit.js
+++ backend/assets/js/controllers/usersEdit.js
@@ -61,3 +61,17 @@ angular.module('campaignsApp.userEdit', [])
         }
 
     })
+    .directive('convertNumber', function() {
+        return {
+            require: 'ngModel',
+            link: function(scope, el, attr, ctrl) {
+                ctrl.$parsers.push(function(value) {
+                    return parseInt(value, 10);
+                });
+
+                ctrl.$formatters.push(function(value) {
+                    return value.toString();
+                });
+            }
+        }
+    });
