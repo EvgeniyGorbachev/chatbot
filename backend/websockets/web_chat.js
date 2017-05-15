4c21675642f7f6fcccf45f46a54980257aef1376
zhenya <webvagus@gmail.com>
2017-05-15 18:31:08 +0300

    remove code

--- backend/websockets/web_chat.js
+++ backend/websockets/web_chat.js
@@ -29,14 +29,6 @@ module.exports = function(webChat) {
             userId: 1
           }
 
-          // Create conversation
-          // db.Conversations.create(conf).then(function(data) {
-          //
-          // }).catch(function(err) {
-          //   console.log(err)
-          //   socket.emit('err', 'Can not create conversation')
-          // })
-
             // console.log('CREATE USER: ', response);
           socket.emit('initUser', response)
 
