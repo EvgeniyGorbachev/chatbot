508985375064d6b3005cfabf18e1c25be7fe21ce
zhenya <webvagus@gmail.com>
2017-05-15 17:48:08 +0300

    fix webhook

--- backend/webhooks/webhooks.js
+++ backend/webhooks/webhooks.js
@@ -6,25 +6,27 @@ const request = require('request')
  */
 exports.webChat = (req, res) => {
   console.log('Get webhookkkkkk: ', req.body);
-  req.webChatSocket.emit('webhook', {type: 'new message', userId: req.body.appUser['_id'], appId: req.body.app['_id'], messages: req.body.messages});
+  if (req.body.trigger == 'message:appMaker') {
+    req.webChatSocket.emit('webhook', {type: 'new message', userId: req.body.appUser['_id'], appId: req.body.app['_id'], messages: req.body.messages});
 
-  // Hack for lambda
-  if (req.body.appUser['_id'] && req.body.app['_id']) {
-    let dataToLambda = {
-      "trigger":"delivery:success",
-      "app":{"_id":req.body.app['_id']},
-      "appUser":{"_id":req.body.appUser['_id']},
-      "destination":{"type":"api"},
-      "messages":[{"text":"emulation"}],
-      "timestamp":1493914595.09
+    // Hack for lambda
+    if (req.body.appUser['_id'] && req.body.app['_id']) {
+      let dataToLambda = {
+        "trigger":"delivery:success",
+        "app":{"_id":req.body.app['_id']},
+        "appUser":{"_id":req.body.appUser['_id']},
+        "destination":{"type":"api"},
+        "messages":[{"text":"emulation"}],
+        "timestamp":1493914595.09
+      }
+
+      request.post({url: process.env.CONFIRM_ORDER_CALLBACK, body: dataToLambda, json:true}, function(err,httpResponse,body){
+        if (err) console.log('Get err from lambda: ', err)
+        console.log('Get response from lambda: ', body)
+      })
     }
 
-    request.post({url: process.env.CONFIRM_ORDER_CALLBACK, body: dataToLambda, json:true}, function(err,httpResponse,body){
-      if (err) console.log('Get err from lambda: ', err)
-      console.log('Get response from lambda: ', body)
-    })
+
+    res.sendStatus(201);
   }
-
-
-  res.sendStatus(201);
 };
