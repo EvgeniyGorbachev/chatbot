23ba2bd811ac99ba931f2ad4989486a300ef0c2a
Mihail Shumilov <mschumilow@gmail.com>
2017-05-23 16:33:00 +0300

    add confirm message

--- backend/assets/js/controllers/dashboard.js
+++ backend/assets/js/controllers/dashboard.js
@@ -29,3 +29,23 @@ angular.module('campaignsApp.dashboard', [])
       // $('form').submit();
     }
   })
+    .directive('ngConfirmClick', [
+        function(){
+            return {
+                link: function (scope, element, attr) {
+                    var msg = attr.ngConfirmClick || "Are you sure?";
+
+                    var clickAction = attr.confirmedClick;
+                    element.bind('click',function (event) {
+
+                        if ( window.confirm(msg) ) {
+                            if(clickAction) {
+                                scope.$eval(clickAction)
+                            }
+                        }else{
+                            event.preventDefault();
+                        }
+                    });
+                }
+            };
+        }]);
